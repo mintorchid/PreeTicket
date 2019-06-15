e8b4d85a2a512e2274b7cd1d95f6466dcf972966
mintorchid <992652081@qq.com>
2019-06-16 05:35:18 +0800

    fix notice import bug

--- PreeTicket-Server/src/main/java/cn/edu/bjtu/preeticket/controller/NoticeController.java
+++ PreeTicket-Server/src/main/java/cn/edu/bjtu/preeticket/controller/NoticeController.java
@@ -2,6 +2,7 @@ package cn.edu.bjtu.preeticket.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import cn.edu.bjtu.preeticket.model.Notice;
 import cn.edu.bjtu.preeticket.service.NoticeService;
