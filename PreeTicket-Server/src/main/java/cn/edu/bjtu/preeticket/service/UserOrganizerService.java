2a9f0db9d4e8a0f8bf565e2ce92f6708ea54f2e5
mintorchid <992652081@qq.com>
2019-06-16 03:58:20 +0800

    organizer init

--- PreeTicket-Server/src/main/java/cn/edu/bjtu/preeticket/service/UserOrganizerService.java
+++ PreeTicket-Server/src/main/java/cn/edu/bjtu/preeticket/service/UserOrganizerService.java
@@ -1,5 +1,16 @@
 package cn.edu.bjtu.preeticket.service;
 
+import java.util.List;
+
+import cn.edu.bjtu.preeticket.model.UserOrganizer;
+
 public interface UserOrganizerService {
 
+    int addUser(UserOrganizer userOrganizer);
+
+    int checkPwd(UserOrganizer userOrganizer);
+
+    UserOrganizer getUserByUsername(String username);
+
+    List<UserOrganizer> getUsers();
 }
