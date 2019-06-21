2b9c02c6dc5ff171bfeea01d27b4f4ef1501b2de
mintorchid <992652081@qq.com>
2019-06-21 19:21:40 +0800

    选座（未完成）吃饭回来再继续写

--- PreeTicket-Server/src/main/java/cn/edu/bjtu/preeticket/service/impl/TicketServiceImpl.java
+++ PreeTicket-Server/src/main/java/cn/edu/bjtu/preeticket/service/impl/TicketServiceImpl.java
@@ -1,9 +1,28 @@
 package cn.edu.bjtu.preeticket.service.impl;
 
+import cn.edu.bjtu.preeticket.mapper.TicketMapper;
+import cn.edu.bjtu.preeticket.model.Ticket;
 import cn.edu.bjtu.preeticket.service.TicketService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TicketServiceImpl implements TicketService {
+    @Autowired
+    private TicketMapper ticketMapper;
 
+    @Override
+    public int chooseSeat(Ticket ticket) {
+        String seat_map  = ticketMapper.getSeatMap(ticket);
+        if (seat_map.charAt(ticket.getSeat()) == '1') {
+            return 0;
+        } else {
+            StringBuilder strBuilder = new StringBuilder(seat_map);
+            strBuilder.setCharAt(ticket.getSeat(), '1');
+            seat_map = strBuilder.toString();
+            //ticketMapper
+            return 1;
+        }
+    }
 }
